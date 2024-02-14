338e54419e2b1705a18a1d5a49c0027ec48af6ad
mr.Hussein Abdul-Ameer <husseinabdulameer11@gmail.com>
2024-02-14 16:44:18 +0100

    Update hashtable.h

--- hashtable.h
+++ hashtable.h
@@ -5,12 +5,12 @@
 
 void initialize_hashtable();
 void insert_variable(const char *name);
-void insert_array(const char *name);
+void insert_array(const char *name, int array_size);
 void update_variable(const char *name, int value);
-void update_array_element(const char *name, int index, int value);
+void update_array_element(const char *name, int index, int value, int array_size);
 int get_variable_value(const char *name);
-int get_array_element_value(const char *name, int index);
+int get_array_element_value(const char *name, int index, int array_size);
 void display_variable(const char *name);
-void display_array_element(const char *name, int index);
+void display_array_element(const char *name, int index, int array_size);
 
 #endif  // HASHTABLE_H
